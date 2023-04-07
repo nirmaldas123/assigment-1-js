c2b9bbf71905f39789ade38ab1a35df7faaedb7a
nirmaldas123 <mrnirmaldas456@gmail.com>
2023-04-07 12:17:18 +0500

    work

---
+++ app.js
@@ -0,0 +1,34 @@
+// alert("helllo world");
+// alert("thanks for your interest");
+// alert("shaman");
+
+// console.log("Ramzan mubarak");
+
+// var nirmal = "dass";
+// var name = "Nirmaljeewani";
+// console.log(nirmal+ " " +name);
+
+// var a=10;
+// var b=20;
+// var c=a-b+a*(a-b);
+// console.log(c);
+
+// var a=10;
+// var b=++a;
+// console.log(a);
+
+// var firstname= prompt("enter first name");
+// console.log(firstname)
+
+// var firstnumber=+prompt("enter first number" ,1)
+// var secondnumber=+prompt("enter second number" ,1)
+// var a = firstnumber+secondnumber;
+// console.log(a);
+
+var name=prompt("Enter your name" ,"nirmal");
+if(name=="nirmal")
+{
+alert("Correct!")
+}
+
+
